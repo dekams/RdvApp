ff5e95ee90230dd31d50244d90964f7413c821a7
Eric Kamay <eric.kamay@ctg.com>
2019-12-06 15:29:23 +0100

    Initial Commit

---
+++ RdvApp.API/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RdvApp.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RdvApp.API.Data
+{
+    public class DataContext: DbContext
+    {
+        public DbSet<Value> Values { get; set; }
+        public DataContext(DbContextOptions<DataContext> options) : base(options) {}
+    }
+}
